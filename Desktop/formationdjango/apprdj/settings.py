ae964771481f4cd724a1d9aa35f7252e7cba90e6
ktbm <boliventtalla12@gmail.com>
2025-08-07 08:31:24 +0100

    jjd

--- Desktop/formationdjango/apprdj/settings.py
+++ Desktop/formationdjango/apprdj/settings.py
@@ -31,8 +31,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://ghittest.onrender.com']
 
+
+#ALLOWED_HOSTS = ['ghittest.onrender.com']
 
 # Application definition
 
